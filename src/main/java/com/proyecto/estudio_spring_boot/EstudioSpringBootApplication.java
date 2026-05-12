32eec2ecc3512ec5101ccfab9005d500f4bd194f
mateo2002xd1 <mateo2002xd1@gmail.com>
2026-05-12 14:56:28 -0500

    Primer proyecto Spring Boot

---
+++ src/main/java/com/proyecto/estudio_spring_boot/EstudioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.estudio_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioSpringBootApplication.class, args);
+	}
+
+}
